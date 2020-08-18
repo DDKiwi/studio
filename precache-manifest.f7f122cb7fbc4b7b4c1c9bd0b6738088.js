4456e357f37a1f80530d32729fcbcb823607b880
LIDHREKLAM\daniel <daniel@lidhreklam.se>
2020-08-18 10:43:56 +0200

    Updates

---
+++ precache-manifest.f7f122cb7fbc4b7b4c1c9bd0b6738088.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e548e9685e344d2b321a0a035f24c8e7",
+    "url": "/studio/index.html"
+  },
+  {
+    "revision": "b4b64da93234aec001a3",
+    "url": "/studio/static/css/main.1c165f0e.chunk.css"
+  },
+  {
+    "revision": "910b11b7809cdb6b4958",
+    "url": "/studio/static/js/2.2920e5c1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/studio/static/js/2.2920e5c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4b64da93234aec001a3",
+    "url": "/studio/static/js/main.380f6491.chunk.js"
+  },
+  {
+    "revision": "d8dd7b0d53e1ed94510d",
+    "url": "/studio/static/js/runtime-main.1056a627.js"
+  },
+  {
+    "revision": "13141fe31a0fc010d0273da411af3fb3",
+    "url": "/studio/static/media/202008.13141fe3.jpg"
+  }
+]);

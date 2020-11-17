6feaee1ecf35ccb03a0fab4ceacc119a1c1cf8a9
LIDHREKLAM\daniel <daniel@lidhreklam.se>
2020-11-17 14:59:31 +0100

    Updates

---
+++ precache-manifest.cf0927694ab11ea1cd151f4d41b36372.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77608dfa0a2112352897a02b36329b16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b9b81b675ca2611e012",
+    "url": "/static/css/main.09f0bfe8.chunk.css"
+  },
+  {
+    "revision": "ea23788f6dc3de92bce5",
+    "url": "/static/js/2.af1a7e9e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.af1a7e9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b9b81b675ca2611e012",
+    "url": "/static/js/main.0fa0c2b8.chunk.js"
+  },
+  {
+    "revision": "ddb23d41b025de166703",
+    "url": "/static/js/runtime-main.60ded422.js"
+  }
+]);
